1072e8fe1ff8010ef5a42d1cfd60e3bff24f184b
erofeevAA <erofeev.aa@students.dvfu.ru>
2022-01-21 11:21:09 +1000

    code refactor

--- utils/BufferModel.java
+++ utils/BufferModel.java
@@ -6,7 +6,7 @@ public class BufferModel {
 
     public int posStart;
     public String str;
-    public int state = 0;
+    public int state;
 
     public BufferModel(int pos, String str, int state) {
         this.posStart = pos;
